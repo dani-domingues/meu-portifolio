c9f4a57691f5a24433b61be5cc82cecddc22b46b
C1324523 Daniela Domingues Queiros de Oliveira <c1324523@interno.bb.com.br>
2024-02-03 21:00:18 -0300

    primeiro commit

---
+++ src/Components/Header/index.jsx
@@ -0,0 +1,14 @@
+function Header() {
+    return (
+      <div className="Header">
+        <li>
+            <ul> Home </ul>
+            <ul> Home </ul>
+            <ul> Home </ul>
+            <ul> Home </ul>
+        </li>
+      </div>
+    );
+  }
+  
+  export default Header;
---
+++ src/Home/Home.jsx
@@ -0,0 +1,13 @@
+import Header from "../Components/Header";
+
+function Home() {
+  return (
+    <div className="Home">
+        <Header></Header>
+        <p> Hello World!</p>
+        <p>Eu sou a Dani Domingues</p>
+    </div>
+  );
+}
+
+export default Home;
